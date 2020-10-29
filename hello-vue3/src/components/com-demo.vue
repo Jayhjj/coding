c6e74db19f38be19e519d7c71f755ecec91cf811
jayhjj <2762319789@qq.com>
2020-10-29 19:59:49 +0800

    vue3

---
+++ hello-vue3/src/components/com-demo.vue
@@ -0,0 +1,14 @@
+<template>
+     <FragmentDemo :data ="['1','2','3','4','5']"/>
+</template>
+<script>
+import FragmentDemo from './fragment.vue'   
+export default {
+    components:{
+        FragmentDemo
+    }
+}
+</script>
+<style>
+
+</style>
