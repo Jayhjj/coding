469db1a85bf4d67078405fc25881b7c537d7a3ec
jayhjj <2762319789@qq.com>
2020-11-26 17:58:57 +0800

    http

---
+++ http/Session,Cookie/index.js
@@ -0,0 +1,24 @@
+
+const Koa = require('koa');
+const path = require('path');
+const router = require('koa-router')();
+const app = new Koa();
+
+router.get('/', async (ctx) => {
+    const fs = require('fs');
+    const html = fs.readFileSync('./index.html', 'utf-8')
+    ctx.body = html;
+})
+router.get('/login', async (ctx) => {
+})
+router.get('/islogin', async (ctx) => {
+})
+
+app
+    .use(router.routes())
+    .use(router.allowedMethods());
+
+app.listen(9090, () => {
+    console.log('server is running 9090');
+});
+
