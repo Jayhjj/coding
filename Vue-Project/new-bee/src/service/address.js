7373ea7cbbf2a7ce6cf00d277307ccb5fde175b1
jayhjj <2762319789@qq.com>
2021-01-14 22:50:09 +0800

    new-bee

---
+++ Vue-Project/new-bee/src/service/address.js
@@ -0,0 +1,8 @@
+import axios from '../utils/axios'
+export function getAddressDetail(id){
+    return axios.get(`/address/${id}`)
+}
+
+export function getDefaultAddress(){
+    return axios.get('/address/default')
+}
--- Vue-Project/new-bee/src/views/Cart.vue
+++ Vue-Project/new-bee/src/views/Cart.vue
@@ -2,7 +2,7 @@
   <div class="cart-box">
     <s-header :name="'购物车'" :back="false"></s-header>
     <!-- 列表 -->
-    <div class="cart-body">
+    <div  v-if="list.length > 0" class="cart-body">
       <van-checkbox-group v-model="result" @change="groupChange">
         <van-swipe-cell v-for="(item,index) in list " :key="index">
             <div class="good-item">
@@ -29,10 +29,10 @@
 
 
     <!-- 合计 -->
-    <van-submit-bar v-if="true" class="submit-all" :price="total*100" button-text="结算" @submit="onSubmit">
+    <van-submit-bar v-if="list.length > 0" class="submit-all" :price="total*100" button-text="结算" @submit="onSubmit">
     <van-checkbox @click="allCheck" v-model="checkAll">全选</van-checkbox>
     </van-submit-bar>
-    <div class="empty" v-if="false">
+    <div class="empty" v-else>
       <img class="empty-cart" src="//s.yezgea02.com/1604028375097/empty-car.png" alt="空购物车">
       <div class="title">购物车空空如也</div>
       <van-button round color="#1baeae" type="primary" @click="goTo" block>前往选购</van-button>
@@ -45,15 +45,17 @@ import sHeader from "@/components/SimpleHeader";
 import { ref, reactive, toRefs, onMounted, computed } from "vue";
 import navBar from "@/components/NavBar";
 import { Toast } from 'vant';
-import {getCart,modifyCart} from '@/service/cart'
+import {getCart,modifyCart,deleteCartItem} from '@/service/cart'
+import {useRouter} from 'vue-router'
 export default {
   components: {
     sHeader,
     navBar
   },
   setup() {
+    const router = useRouter()
     const state = reactive({
-      result: [],
+      result: [], //想买的，勾选的
       list:[],
       checkAll:true
     });
@@ -63,7 +65,7 @@ export default {
     const init = async () => {
       Toast.loading({message:'加载中...',forbidClick: true})
       const {data} = await getCart({pageNumber:1})
-      console.log(data);//购物车里面有几条数据
+      // console.log(data);//购物车里面有几条数据
       state.list = data//装数据
       state.result = data.map(item => item.cartItemId)
     //   console.log(state.result);reslut里面存放的是购物车列表产品id
@@ -99,8 +101,8 @@ export default {
     })
     //计数器增加，增减数量
     const numChange = async(value,detail) => {
-        console.log(value,detail);
-        console.log(state.list);
+        // console.log(value,detail);
+        // console.log(state.list);
         if(state.list.filter(item => item.cartItemId == detail.name)[0].goodsCount == value){
             //从购物车列表过滤出来加减的商品,设置其值
             return
@@ -121,12 +123,46 @@ export default {
         Toast.clear()
 
     }
+
+    //删除购物车商品
+    const deleteGood = async (goodId) => {
+      const {resultCode} = await deleteCartItem(goodId)
+      // console.log(id);
+      if(resultCode === 200){
+        state.list.forEach((item,index) => {
+          if(item.cartItemId == goodId){
+            state.list.splice(index,1)
+          }
+        })
+      }else{
+        Toast({
+          message:'出错了',
+          forbidClick:  true
+        });
+      }
+    }
+    //前往选购
+    const goTo = () => {
+      router.push({path: '/home'})
+    }
+    //结算
+    const onSubmit = () => {
+      if(state.result.length == 0){
+        Toast.fail('请选择商品进行结算')
+        return
+      }
+      const params = JSON.stringify(state.result)
+      router.push({path:'/create-order',query:{cartItemId: params}})//路由传参
+    }
     return {
       ...toRefs(state),
       allCheck,
       total,
       groupChange,
-      numChange
+      numChange,
+      deleteGood,
+      goTo,
+      onSubmit
     };
   }
 };
