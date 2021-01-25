e0a68dd9f768e7da75c0e2e8f66a887506ad3d5d
jayhjj <2762319789@qq.com>
2021-01-25 22:42:13 +0800

    leetcode

--- leetcode/支持增量操作的栈/1.js
+++ leetcode/支持增量操作的栈/1.js
@@ -2,3 +2,23 @@ var CustomStack = function (maxSize) {
     this.list = [];
     this.maxSize = maxSize
 }
+
+CustomStack.prototype.push = function (x) {
+    if(this.list.length < this.maxSize){
+        this.list.push(x)
+    }
+}
+
+CustomStack.prototype.pop = function() {
+    if(this.list.length){
+        return this.list.pop()
+    }else{
+        return -1
+    }
+}
+
+CustomStack.prototype.increment = function (k, val) {
+  for (let i = 0; i < k && i < this.list.length; i++) {
+    this.list[i] += val
+  }
+};
