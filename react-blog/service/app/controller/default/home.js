280bed73ccf7a83d9419e3e94db63c13af5ea980
jayhjj <2762319789@qq.com>
2021-03-12 20:54:28 +0800

    blog

--- react-blog/service/app/controller/default/home.js
+++ react-blog/service/app/controller/default/home.js
@@ -5,7 +5,9 @@ const Controller = require('egg').Controller
 class HomeController extends Controller{
 
     async index(){
-        this.ctx.body="api接口"
+        let result = await this.app.mysql.get("blog_content",{})
+        console.log(result)
+        this.ctx.body= result
     }
 }
 
