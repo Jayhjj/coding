feada93fbee85cb8a55a2dd76c112e770c113894
jayhjj <2762319789@qq.com>
2021-03-18 14:13:23 +0800

    myBlog

---
+++ myBlog/admin/src/config/apiUrl.js
@@ -0,0 +1,9 @@
+let ipUrl = 'http://127.0.0.1:7001/admin/' 
+
+let servicePath = {
+    checkLogin: ipUrl + 'checkLogin',
+    getTypeInfo: ipUrl + 'getTypeInfo',
+    addArticle:ipUrl + 'addArticle' , 
+    updateArticle:ipUrl + 'updateArticle' ,  //  修改文章第api地址
+}
+export default servicePath
